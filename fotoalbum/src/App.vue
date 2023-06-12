160a16baf2dc87fd6590f7e4c46f6a8dd3e4d6c1
domenicatonziello <domenica.tonziello10@gmail.com>
2023-06-12 12:30:11 +0200

    home page user

--- fotoalbum/src/App.vue
+++ fotoalbum/src/App.vue
@@ -1,16 +1,67 @@
 <script>
+import axios from 'axios';
+const base_url = "http://localhost:8080/api/foto"
+import FotoCard from './components/FotoCard.vue';
 export default {
+  name: "AppAlbum",
   data() {
     return {
-      name: "AppAlbum",
+      foto: null,
+      searchTerm: ""
     }
+  },
+  components: { FotoCard },
+  methods: {
+    fetchFoto() {
+      axios.get(base_url).then(res => {
+        this.foto = res.data;
+      }).catch(e => {
+        console.log(e);
+      })
+    },
+
+    filterFoto(searchTitle) {
+      axios.get(base_url + "/filter", { params: { title: searchTitle } }).then(res => {
+        this.foto = res.data;
+      }).catch(e => {
+        console.log(e);
+      })
+    }
+  },
+  mounted() {
+    this.fetchFoto()
   }
 }
-
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <!-- HEADER -->
+  <header class="container my-3">
+    <nav class="navbar">
+      <h1 class="text-danger">Album</h1>
+      <a href="http://localhost:8080" class="btn btn-outline-success">Accedi</a>
+    </nav>
+  </header>
+
+  <!-- MAIN -->
+  <main class="container">
+    <div class="row mb-5">
+      <div class="col-4 ms-auto">
+        <!-- SEARCH -->
+        <div class="input-group">
+          <input @keyup.enter="filterFoto(searchTerm)" type="text" class="form-control"
+            placeholder="Filtra per titolo foto..." v-model="searchTerm">
+          <button @click="filterFoto(searchTerm)" class="btn btn-outline-secondary" type="button">Filtra</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <FotoCard v-for="foto in foto" :foto="foto" :key="foto.id" />
+    </div>
+
+
+  </main>
 </template>
 
 <style scoped></style>
---
+++ fotoalbum/src/components/FotoCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'FotoCard',
+    data() {
+        return {
+
+        }
+    },
+    props: { foto: Object },
+    mounted() {
+        console.log(this.foto);
+    }
+}
+</script>
+
+<template>
+    <div class="card me-5 p-0" style="width: 20rem;">
+        <img :src="foto.url" class="card-img-top" :alt="foto.name">
+        <div class="card-body">
+            <h3 class="card-title"> {{ foto.title }}</h3>
+            <p class="card-text"> {{ foto.description }}</p>
+            <p class="card-text"> <strong>Categorie: </strong> </p>
+            <ul v-if="foto.categories.length > 0">
+                <li v-for="category in foto.categories"> {{ category.name }} </li>
+            </ul>
+            <small v-else>Non ci sono categorie </small>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
